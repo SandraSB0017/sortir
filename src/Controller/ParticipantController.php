bfc0624c6af67fc595da9a74d4331fb1b50c7ba6
Mathildebrbd <barbaud.mat@gmail.com>
2022-04-12 10:25:18 +0200

    creation controller participant + sortie ajout monProfil à la nav création monProfil.html.twig

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    /**
+     * @Route("/participant", name="app_participant")
+     */
+    public function index(): Response
+    {
+        return $this->render('participant/index.html.twig', [
+            'controller_name' => 'ParticipantController',
+        ]);
+    }
+
+    /**
+     * @Route("/monProfil", name="participant_monProfil")
+     */
+    public function monProfil()
+    {
+        return $this->render('participant/monProfil.html.twig');
+    }
+}
