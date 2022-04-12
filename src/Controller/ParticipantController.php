c3331aa718b1dd0fc72c0f7558f4f84fa374d097
Sandra <sbarteau@gmail.com>
2022-04-12 15:51:18 +0200

    sauvegarde et annule profil

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\RegistrationFormType;
 use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,15 +25,44 @@ class ParticipantController extends AbstractController
     /**
      * @Route("/monProfil/{id}", name="participant_monProfil")
      */
-    public function monProfil(int $id, ParticipantRepository $participantRepository, Request $request): Response
+    public function monProfil(int $id,
+                              ParticipantRepository $participantRepository,
+                              Request $request,
+                              EntityManagerInterface $entityManager
+    ): Response
     {
         $participant = $participantRepository->find($id);
 
         $form = $this->createForm(RegistrationFormType::class, $participant);
         $form->handleRequest($request);
 
+
+
+
+
+            if( $form->get('saveAndAdd')->isClicked() && $form->isValid())
+
+            {
+               $entityManager->persist($participant);
+               $entityManager->flush();
+
+               $this->addFlash('succes', 'Profil modifié');
+
+               return $this->redirectToRoute('app_accueil');
+
+            }/*else if($form->get('annuler')->isClicked())
+
+            {
+                return $this->redirectToRoute('participant_monProfil',
+                    ['id'=>$participant->getId()]
+
+                );
+            }*/
+
+
+
         return $this->render('participant/monProfil.html.twig',[
-            "participant"=>$participant,
+            'participant'=>$participant,
             'editForm' => $form->createView(),
         ]);
     }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
@@ -60,6 +61,12 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+           ->add('saveAndAdd', SubmitType::class,[
+                'label'=>'Sauvegarder'
+            ])
+           /*->add('annuler', SubmitType::class,[
+                'label'=>'Annuler'
+            ])*/
         ;
     }
 
